4711e79b9931803c04c65da3fdcc3a1de47297a4
Felipe Muniz <lipe.baterra@gmail.com>
2022-12-09 00:32:51 -0300

    Inicio do projeto

---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import "./HomePage.css"
+
+const HomePage = () => {
+  return (
+    <div className="start">
+        <h1>Palavra Secreta</h1>
+        <p>Clique no botão abaixo para iniar o jogo</p>
+
+        <button>Iniciar</button>
+    </div>
+  )
+}
+
+export default HomePage
